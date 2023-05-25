3622b7d5c464e01e70a8352b03423381b8630b5c
allexcho <allexcho98@gmail.com>
2023-05-24 21:32:03 -0300

    Mi primer commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+
+app.use(express.static("public"))
+
+const port = process.env.PORT || 3030;
+app.listen(port, () => console.log('Servidor corriendo en el puerto ${port}'));
+
+/*
+app.listen(3030, () => {
+    console.log("Servidor corriendo en el 3010")
+})
+*/
+
+app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
