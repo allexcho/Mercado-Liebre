0d38758e02ddd10e19c21d71100a29ef80733944
allexcho <allexcho98@gmail.com>
2023-06-21 21:56:22 -0300

    modificaciones

---
+++ src/middleware/logMiddleware.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+
+const datos = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../database/user.json")));
+
+const logMiddleware = (req, res, next) => {
+    const usuario = datos.find((row) => row.id == req.params.id);
+
+    if(usuario.loggeado == true) {
+        next()
+    } else {
+        return res.redirect("/user/login")
+    }
+}
+
+module.exports = logMiddleware
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,6 +4,8 @@ const userController = require('../controllers/userController');
 const multer = require('multer');
 const path = require('path');
 
+const logMiddleware = require("../middleware/logMiddleware")
+
 const multerDiskStorage = multer.diskStorage({ 
     destination: (req,file,cb) => {
         cb(null, path.resolve(__dirname, '../../public/img'))
@@ -25,7 +27,7 @@ router.post('/register', userController.processRegister)
 // Edicion de usuario 
 router.get('/editar/:id', userController.editar);
 router.put('/editar/:id', fileUpload.single('image'), userController.processEdit)
-router.get('/perfil/:id', userController.perfil);
+router.get('/perfil/:id', logMiddleware, userController.perfil);
 
 // Eliminar usuario
 router.delete('/eliminar/:id', userController.eliminar);
