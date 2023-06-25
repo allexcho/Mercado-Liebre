b34e129e9e1b8f5f59ee0443030356124f303737
allexcho <allexcho98@gmail.com>
2023-06-25 20:56:53 -0300

    modi header

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const controller = require("../controllers/mainController");
 
 router.get("/", controller.home);
+/*
 router.get("/register", controller.register)
 router.get("/login", controller.login)
+*/
 
 module.exports = router;
